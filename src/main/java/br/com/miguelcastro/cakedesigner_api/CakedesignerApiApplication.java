99f6d465d7ac1cd72a1bcaf0fe4792342a43b283
miguelscastro <miguelscastro18@gmail.com>
2025-05-08 16:21:26 -0300

    chore: initial commit with project scaffold

---
+++ src/main/java/br/com/miguelcastro/cakedesigner_api/CakedesignerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.miguelcastro.cakedesigner_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakedesignerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakedesignerApiApplication.class, args);
+	}
+
+}
