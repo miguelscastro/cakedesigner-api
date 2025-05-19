21635cd930e7548eb170a03cf5035e6c9559f80d
miguelscastro <miguelscastro18@gmail.com>
2025-05-19 09:40:15 -0300

    feat(config): add global CORS configuration

--- src/main/java/br/com/miguelcastro/cakedesigner_api/modules/product/controllers/ProductController.java
+++ src/main/java/br/com/miguelcastro/cakedesigner_api/modules/product/controllers/ProductController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -38,7 +37,6 @@ public class ProductController {
         }
     }
 
-    @CrossOrigin(origins = "http://127.0.0.1:3000", allowedHeaders = "*")
     @GetMapping("/")
     public ResponseEntity<List<ProductResponseDTO>> list() {
         List<ProductEntity> products = productRepository.findAll();
