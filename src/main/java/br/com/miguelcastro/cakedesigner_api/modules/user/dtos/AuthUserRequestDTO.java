77f4c310fd4215500b4120991269cd4c797dddcb
miguelscastro <miguelscastro18@gmail.com>
2025-05-28 12:18:44 -0300

    chore: add rules on DTOs

--- src/main/java/br/com/miguelcastro/cakedesigner_api/modules/user/dtos/AuthUserRequestDTO.java
+++ src/main/java/br/com/miguelcastro/cakedesigner_api/modules/user/dtos/AuthUserRequestDTO.java
@@ -1,5 +1,9 @@
 package br.com.miguelcastro.cakedesigner_api.modules.user.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +12,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthUserRequestDTO {
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "E-mail inválido")
     private String email;
+
+    @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 8, message = "A senha deve ter no mínimo 8 caracteres")
+    @Pattern(regexp = ".*[A-Z].*", message = "A senha deve conter pelo menos uma letra maiúscula")
+    @Pattern(regexp = ".*[a-z].*", message = "A senha deve conter pelo menos uma letra minúscula")
+    @Pattern(regexp = ".*\\d.*", message = "A senha deve conter pelo menos um número")
+    @Pattern(regexp = ".*[^A-Za-z0-9].*", message = "A senha deve conter pelo menos um caractere especial")
     private String password;
 }
--- src/main/java/br/com/miguelcastro/cakedesigner_api/modules/user/dtos/CreateUserRequestDTO.java
+++ src/main/java/br/com/miguelcastro/cakedesigner_api/modules/user/dtos/CreateUserRequestDTO.java
@@ -1,10 +1,21 @@
 package br.com.miguelcastro.cakedesigner_api.modules.user.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CreateUserRequestDTO {
+    @NotBlank(message = "O nome é obrigatório")
+    @Size(min = 2, max = 100, message = "O nome deve ter entre 2 e 100 caracteres")
     private String name;
+
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "E-mail inválido")
     private String email;
+
+    @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 6, message = "A senha deve ter pelo menos 6 caracteres")
     private String password;
 }
