702dae2364b2ff9ad14d0761b43c290742a14c55
miguelscastro <miguelscastro18@gmail.com>
2025-05-28 13:28:29 -0300

    feat: handle errors on controllers

---
+++ src/main/java/br/com/miguelcastro/cakedesigner_api/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,11 @@
+package br.com.miguelcastro.cakedesigner_api.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+    private String message;
+    private String field;
+}
