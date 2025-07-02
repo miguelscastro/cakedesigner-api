0e950c182f5491233ab301dc271eb4191ca67d08
miguelscastro <miguelscastro18@gmail.com>
2025-07-02 16:16:03 -0300

    chore: allow front-end deployed origin

--- src/main/java/br/com/miguelcastro/cakedesigner_api/security/CorsConfig.java
+++ src/main/java/br/com/miguelcastro/cakedesigner_api/security/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000", "https://cakedesigner.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
